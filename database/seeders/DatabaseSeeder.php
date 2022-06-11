c9224d5d79352ff679b30a4735881258ff63588f
Grigory Tydykov <tydykov2022@mail.ru>
2022-06-11 18:14:05 +0700

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ComplaintReason::class,
+            ComplaintReasonSeeder::class,
             PolyclinicSeeder::class,
         ]);
     }
